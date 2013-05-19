22584bb2f302b7171a1276f824820de69bfe16ed
Spencer Montgomery <smontgo5@gmail.com>
2013-05-19 01:13:56 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Turdmuffin::Application.config.secret_key_base = '1c4312b9e3451ca869894467d331c396951d97aa91df1c3eeec2b27f56aeafe2bce7fe6881830267bea5845ed33a68a16ba1008a6b8ea493a70706978afcd624'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Turdmuffin::Application.config.session_store :cookie_store, key: '_turdmuffin_session'
